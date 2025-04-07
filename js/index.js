ae1594c98c7650d2a43151fb3e7ef44c9440d1cb
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2025-04-06 21:24:41 -0300

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let animais = [];
+console.log(animais);
+animais.push("Cachorro", "Gato", "Passarinho");
+console.log(animais);
+
+animais.shift();
+console.log(animais);
+
+animais.unshift("Jacaré");
+console.log(animais);
+
+animais[1] = "Girafa";
+console.log(animais);
+
+let frutas = ["Manga", "Banana", "Maçan"];
+console.log(frutas.length);
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);    
+}
+
