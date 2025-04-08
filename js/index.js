63d378e490d051947fea4b11e3e34952935f1baa
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2025-04-08 16:14:16 -0300

    carrossel
    carrossel

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 // CARROSSEL
-const track = document.getElementById('carousel-track');
-const images = Array.from(track.children);
+    const track = document.getElementById('carouselTrack');
+    const nextBtn = document.getElementById('nextBtn');
+    const prevBtn = document.getElementById('prevBtn');
+    let index = 0;
 
-// Duplicar imagens
-images.forEach(img => {
-  const clone = img.cloneNode(true);
-  track.appendChild(clone);
-});
+    nextBtn.addEventListener('click', () => {
+      if (index < track.children.length - 3) {
+        index++;
+        updateCarousel();
+      }
+    });
+
+    prevBtn.addEventListener('click', () => {
+      if (index > 0) {
+        index--;
+        updateCarousel();
+      }
+    });
+
+    function updateCarousel() {
+      const slideWidth = track.children[0].offsetWidth;
+      track.style.transform = `translateX(-${slideWidth * index}px)`;
+    }
+
+    window.addEventListener('resize', updateCarousel);
 // CARROSSEL FIM
